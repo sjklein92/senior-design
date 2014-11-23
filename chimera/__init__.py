fdb8e2588401efef9d0509b9920f1fa4ea7b1c0d
Michael Limiero <mike5713@gmail.com>
2014-11-23 17:24:56 -0500

    PUT and DELETE files

--- chimera/__init__.py
+++ chimera/__init__.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, abort, json, send_file
+from flask import Flask, render_template, abort, json, send_file, request
 import os
 
 app = Flask(__name__, instance_relative_config=True)
@@ -25,6 +25,11 @@ def make_json(data, status=200, headers={}):
     default_headers.update(headers)
     return json.dumps(data), status, default_headers
 
+def make_plain(data, status=200, headers={}):
+    default_headers = {"Content-Type": "text/plain"}
+    default_headers.update(headers)
+    return data, status, default_headers
+
 @app.route('/api/files')
 @auth.login_required
 def files_index():
@@ -40,6 +45,28 @@ def files_get(path):
         return abort(404)
     return send_file(path, 'text/plain')
 
+@app.route('/api/files/<path:path>', methods=['PUT'])
+@auth.login_required
+def files_put(path):
+    path = os.path.abspath(os.path.join(auth.current_user.folder_path, path))
+    if not path.startswith(auth.current_user.folder_path) or not path.startswith(app.config['STORAGE_PATH']):
+        return abort(403)
+    with open(path, 'wb') as f:
+        f.write(request.data)
+    return make_plain('Updated')
+
+@app.route('/api/files/<path:path>', methods=['DELETE'])
+@auth.login_required
+def files_delete(path):
+    path = os.path.abspath(os.path.join(auth.current_user.folder_path, path))
+    if not path.startswith(auth.current_user.folder_path) or not path.startswith(app.config['STORAGE_PATH']):
+        return abort(403)
+    if not os.path.exists(path):
+        return abort(404)
+    os.remove(path)
+    return make_plain('Removed')
+
+@app.route('/preview/', defaults={'path':'index.html'})
 @app.route('/preview/<path:path>')
 @auth.login_required
 def preview(path):
@@ -51,9 +78,3 @@ def preview(path):
     if not os.path.exists(path):
         return abort(404)
     return send_file(path)
-
-@app.route('/preview/')
-@auth.login_required
-def preview_base():
-    return preview('index.html')
-
