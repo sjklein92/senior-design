6cb206c540b50fbdc78933e86e5bcff6ea265b26
Michael Limiero <mike5713@gmail.com>
2014-11-29 20:09:19 -0500

    Keep track of path and write files

--- chimera/static/js/application.js
+++ chimera/static/js/application.js
@@ -1,3 +1,18 @@
+var editor;
+var editorPath = "";
+var editorIsDirty = false;
+
+function setupEditor(id) {
+    editor = ace.edit(id);
+    editor.setTheme("ace/theme/monokai");
+    editor.on('change', function(e) {
+        if (editorPath) editorIsDirty = true;
+    });
+    editor.on('blur', function(e) {
+        saveDocument();
+    });
+}
+
 function getDocument(path) {
     var modes = {"markdown": "ace/mode/markdown",
                  "md": "ace/mode/markdown",
@@ -17,15 +32,56 @@ function getDocument(path) {
     }
     if (!mode) mode = "ace/mode/plain_text";
 
+    saveDocument();
+    beforeLoading();
+
     $.ajax("/api/files"+path, {
         "cache": false,
         "success": function(data) {
             editor.setValue(data);
             editor.getSession().setMode(mode);
+            editorPath = path;
+            editorIsDirty = false;
+            $("title").text(path + " - Chimera");
+            $("#nav-title").text(path);
         },
         "error": function(data) {
-            editor.setValue("there was an error");
+            editor.setValue("Could not load file");
             editor.getSession().setMode("ace/mode/plain_text");
+            editorPath = "";
+            editorIsDirty = false;
+            $("title").text("Chimera");
+            $("#nav-title").text("Chimera");
         }
     });
+}
+
+function saveDocument() {
+    if (!editorPath || !editorIsDirty) return;
+    var data = editor.getValue();
+
+    editorIsDirty = false;
+
+    $.ajax("/api/files"+editorPath, {
+        "type": "PUT",
+        "data": data,
+        "contentType": "text/plain",
+        "processData": false,
+        "success": function(data) {
+            console.log("Wrote "+editorPath);
+        },
+        "error": function(data) {
+            console.log("Failed to write "+editorPath);
+            editorIsDirty = true;
+        }
+    });
+}
+
+function beforeLoading() {
+    $("title").text("Chimera");
+    $("#nav-title").text("Chimera");
+    editorPath = "";
+    editorIsDirty = false;
+    editor.setValue("Loading...");
+    editor.getSession().setMode("ace/mode/plain_text");
 }
